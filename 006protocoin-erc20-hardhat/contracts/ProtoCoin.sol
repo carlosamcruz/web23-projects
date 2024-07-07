b285575a8ebd66a43fdefdefd9846e2ac3ca6034
carlos <carlosamcruz@ufam.edu.br>
2024-07-06 22:58:20 -0300

    M2-L6-Aula5

--- 006protocoin-erc20-hardhat/contracts/ProtoCoin.sol
+++ 006protocoin-erc20-hardhat/contracts/ProtoCoin.sol
@@ -5,5 +5,33 @@ pragma solidity ^0.8.24;
 // import "hardhat/console.sol";
 
 contract ProtoCoin {
-    
+    string public name = "ProtoCoin";
+    string public symbol = "PRC";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 21000 * 10 ** decimals;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    mapping (address => uint256) private _balances;
+
+    constructor(){
+        _balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance){
+        return _balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require(balanceOf(msg.sender) >= _value, "Insuficient balance.");
+
+        _balances[msg.sender] -= _value;
+        _balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true; 
+    }
+
 }
--- 006protocoin-erc20-hardhat/test/ProtoCoin.test.ts
+++ 006protocoin-erc20-hardhat/test/ProtoCoin.test.ts
@@ -14,10 +14,72 @@ describe("ProtoCoin Tests", function () {
     return { protoCoin, owner, otherAccount };
   }
 
-  it("Should test", async function () {
+  it("Should have correct name", async function () {
     const { protoCoin, owner, otherAccount } = await loadFixture(deployProtoCoinFixture);
 
-    expect(true).to.equal(true);
+    const name = await protoCoin.name();
+
+    expect(name).to.equal("ProtoCoin");
   });
+
+  it("Should have correct symbol", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployProtoCoinFixture);
+
+    const symbol = await protoCoin.symbol();
+
+    expect(symbol).to.equal("PRC");
+  });
+
+  it("Should have correct decimals", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployProtoCoinFixture);
+
+    const decimals = await protoCoin.decimals();
+
+    expect(decimals).to.equal(18);
+  });
+
+  it("Should have correct totalSupply", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployProtoCoinFixture);
+
+    const totalSupply = await protoCoin.totalSupply();
+
+    expect(totalSupply).to.equal(21000n * 10n ** 18n);
+  });
+
+  it("Should get balance", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployProtoCoinFixture);
+
+    const balance = await protoCoin.balanceOf(owner.address);
+
+    expect(balance).to.equal(21000n * 10n ** 18n);
+  });
+
+  it("Should transfer", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployProtoCoinFixture);
+
+    const balanceOwnerBefore = await protoCoin.balanceOf(owner.address);
+    const balanceOtherBefore = await protoCoin.balanceOf(otherAccount.address);
+
+    await protoCoin.transfer(otherAccount, 1n);
+
+    const balanceOwnerAfter = await protoCoin.balanceOf(owner.address);
+    const balanceOtherAfter = await protoCoin.balanceOf(otherAccount.address);
+
+    expect(balanceOwnerBefore).to.equal(21000n * 10n ** 18n);
+    expect(balanceOwnerAfter).to.equal(21000n * 10n ** 18n - 1n);
+
+    expect(balanceOtherBefore).to.equal(0n);
+    expect(balanceOtherAfter).to.equal(1);
+  });
+
+
+  it("Should NOT transfer", async function () {
+    const { protoCoin, owner, otherAccount } = await loadFixture(deployProtoCoinFixture);
+
+    const instance = protoCoin.connect(otherAccount);
+    await expect(instance.transfer(owner, 1))
+      .to.be.revertedWith("Insuficient balance.");
+  });
+
 
 });
